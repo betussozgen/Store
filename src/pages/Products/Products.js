85d47a2527741192498f4955cecb32c9114c2c50
betussozgen <fatmabetulozgen@hotmail.com>
2023-05-10 15:15:43 +0300

    IntegrationOfLottiePackageIntoErrorAndLoadingPages

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -6,6 +6,8 @@ import Config from "react-native-config";
 
 import ProductCard from '../../components/ProductCard';
 import useFetch from "../../hooks/useFetch/useFetch";
+import Loading from "../../components/Loading/Loading";
+import Error from "../../components/Error/Error";
 
 
 function Products() {
@@ -17,10 +19,12 @@ function Products() {
 
     const renderProduct = ({ item }) => <ProductCard product={item} />;
 
+
+
     if (loading) {
-        return <ActivityIndicator size="large" />
+        return <Loading />
     } if (error) {
-        return <Text>{error}</Text>
+        return <Error />
     }
 
     return (
