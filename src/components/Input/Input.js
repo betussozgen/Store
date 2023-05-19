e378c80f1c90bb141785ad34c8697368e68b1337
betussozgen <fatmabetulozgen@hotmail.com>
2023-05-19 16:31:21 +0300

    Formik

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,12 +2,13 @@ import React from "react";
 import { TextInput, View } from "react-native";
 import styles from "./Input.style";
 
-const Input = ({ placeholder, onType }) => {
+const Input = ({ placeholder, value, onType }) => {
     return (
         <View style={styles.container}>
             <TextInput
                 placeholder={placeholder}
-                onChangeText={onType} />
+                onChangeText={onType}
+                value={value} />
         </View>
     )
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,22 +1,42 @@
 import React from "react";
 import { SafeAreaView, Text, View, Image } from "react-native";
+import { Formik } from "formik";
 import styles from './Login.style';
 
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 
 const Login = () => {
+    function handleLogin(values) {
+        console.log(values);
+
+    }
+    // console.log('RENDER');
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.logo_container}>
                 <Image style={styles.logo} source={require('../../asset/login-logo.png')} />
 
             </View>
-            <View style={styles.body_container}>
-                <Input placeholder="Kullanıcı adını giriniz..." />
-                <Input placeholder="Şifrenizi giriniz..." />
-                <Button text="Giriş Yap" />
-            </View>
+            <Formik
+                initialValues={{ username: '', password: '' }}
+                onSubmit={handleLogin}>
+                {({ handleSubmit, handleChange, values }) => (
+                    <View style={styles.body_container}>
+                        <Input
+                            placeholder="Kullanıcı adını giriniz..."
+                            value={values.username}
+                            onType={handleChange('username')} />
+                        <Input
+                            placeholder="Şifrenizi giriniz..."
+                            value={values.password}
+                            onType={handleChange('password')} />
+                        <Button text="Giriş Yap"
+                            onPress={handleSubmit} />
+                    </View>
+                )}
+
+            </Formik>
         </SafeAreaView>
     )
 }
