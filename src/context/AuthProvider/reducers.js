481632e0fbe57ea30935c14cb9896435ade9f02e
betussozgen <fatmabetulozgen@hotmail.com>
2023-05-29 12:27:57 +0300

    created context pages

---
+++ src/context/AuthProvider/reducers.js
@@ -0,0 +1,10 @@
+export default function reducers(state, action) {
+    switch (action.type) {
+        case "SET_USER":
+            const { user } = action.payload;
+            return { ...state, user };
+
+        default:
+            return state;
+    }
+}
